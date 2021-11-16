c2311aad8e6160ad21fb8a7410f057b03d51e7c6
Josh Lapham <jlapham@nimble.com.au>
2021-11-16 13:43:59 +1000

    Fetch recipes on App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import RecipeList from "./RecipeList";
 import Search from "./Search";
 
 const App = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch('/recipes.json')
+      .then(response => response.json())
+      .then(data => {
+        setRecipes(data.recipes);
+      })
+  }, []);
+
   return (
-  <div id="the-app">
-    <header className="app-header">
-      <h1>Recipe Hero</h1>
-    </header>
-    <main>
-      <Search />
-      <RecipeList />
-    </main>
-  </div>
+    <div id="the-app">
+      <header className="app-header">
+        <h1>Recipe Hero</h1>
+      </header>
+      <main>
+        <Search />
+        <RecipeList recipes={recipes} />
+      </main>
+    </div>
   )
 }
 
