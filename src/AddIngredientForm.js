1e8e8eed4f7266c85d13b46c21d49747b29b5029
Josh Lapham <jlapham@nimble.com.au>
2021-11-17 14:01:09 +1000

    Continue implementing Add Ingredient form

--- src/AddIngredientForm.js
+++ src/AddIngredientForm.js
@@ -4,12 +4,18 @@ import { faPlus } from "@fortawesome/free-solid-svg-icons"
 // TODO: Handle `disabled` for `option` elements
 
 const AddIngredientForm = ({ ingredients }) => {
+  const addHandler = (event) => {
+    event.preventDefault();
+    console.log(`[addHandler] event: ${event.target[0].value}`);
+    // TODO: Finish implementing
+  }
+
   return (
-    <form className="ingredient-picker">
+    <form className="ingredient-picker" onSubmit={addHandler}>
       <select placeholder="Add an ingredient">
-        <option disabled="">Pick an ingredient</option>
+        <option disabled={true}>Pick an ingredient</option>
         {ingredients.map(({ title }, index) => (
-          <option>{title}</option>
+          <option key={`${index}-${title}`}>{title}</option>
         ))}
       </select>
       <button>
